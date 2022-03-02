c4cd977dd5f00703bca5c237494d342834e4c490
X404-ch1nVR46 <chinmay.sinha.vr@gmail.com>
2022-03-02 21:08:44 +0530

    Adding sqlite3 support for the authentication

--- users.py
+++ users.py
@@ -1,7 +1,33 @@
 # Creating a User Class
 
+import sqlite3
+
 class Users:
+    
+    TABLE_NAME = 'users'
+
     def __init__(self,_id,username,password):
         self.id = _id
         self.username = username
         self.password = password
+
+
+    def find_by_username(self,username):
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query= "SELECT * FROM {table} WHERE username = ?".format(table = self.TABLE_NAME)
+
+        cursor.execute(query, (username,))
+
+
+class UserRegister:
+
+    def getUserNameByMapping():
+        userlist = ['A','B']
+        for user in userlist:
+            if user.name == "key":
+            # Replacing the user password and user name
+            
+                user.password = 'something else'
